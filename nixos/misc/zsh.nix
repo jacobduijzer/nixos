9aa36b6eea3488539dee03d990bf70dbaca49ea4
Jacob Duijzer <jacob@duijzer.com>
2023-09-23 16:07:12 +0200

    add p10k theme

--- nixos/misc/zsh.nix
+++ nixos/misc/zsh.nix
@@ -1,13 +1,22 @@
 { config, pkgs, ... }:
+let 
+	p10kTheme = ./p10k-theme.zsh;
+in
 {
 	programs.zsh = {
 		enable = true;
 
+		initExtra = ''
+			[[ ! -f ${p10kTheme} ]] || source ${p10kTheme}
+		'';
 		zplug = {
 			enable = true;
 			plugins = [
 				{ name = "zsh-users/zsh-autosuggestions"; }
-				{ name = "romkatv/powerlevel10k"; }
+				{
+					name = "romkatv/powerlevel10k"; 
+					tags = [ as:theme depth:1 ];
+				}
 			];
 		};
 	};
