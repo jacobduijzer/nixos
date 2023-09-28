5a3ba2400b1ee0d5c4ddc4c700c64007d9b2dc2e
Jacob Duijzer <jacob@duijzer.com>
2023-09-28 15:53:36 +0200

    Mako anchor to center screen

--- nixos/desktop/mako.nix
+++ nixos/desktop/mako.nix
@@ -3,14 +3,15 @@
   programs.mako = {
     enable = true;
     defaultTimeout = 5000;
+    anchor = "top-center";
 
     # style
     font = "JetBrainsMono Nerd Font 14";
     backgroundColor = "#ffffff";
     textColor = "#000000";
     borderColor = "#FF8E0D";
-    width = 500;
+    width = 600;
     height = 200;
-    borderRadius = 10;
+    borderRadius = 0;
   };
 }
